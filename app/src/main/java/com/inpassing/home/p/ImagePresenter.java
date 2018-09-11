8eaeba9c06907533ce081d875729dcd978ee3c07
caomei <17603011340@163.com>
2018-09-11 17:09:58 +0800

    del load dialog

--- app/src/main/java/com/inpassing/home/p/ImagePresenter.java
+++ app/src/main/java/com/inpassing/home/p/ImagePresenter.java
@@ -13,8 +13,8 @@ public class ImagePresenter extends HttpSub implements BasePresenter<CallbackSub
 	@Override
 	public void post(CallbackSub<ImageCategory> imageCategoryCallbackSub) {
 		APIurl apIurl = RetrofitClient.retrofit(APIurl.IMAGER_URL, "0").create(APIurl.class);
-		Observable<ImageCategory> observable = apIurl.imageCategory("27d21e7ae0ea1", "44133120e3aef3144aecdaa9c3cf7f6d",
-				"1", "1");
+		Observable<ImageCategory> observable = apIurl.imageCategory("23321", "2eab5093db5c4f2b9568f32286357c94", "1",
+				"1");
 		toSubscribe(observable, imageCategoryCallbackSub);
 	}
 
